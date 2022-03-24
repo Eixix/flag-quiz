950e59ecf2a892e87e6ee73fde96290c69386696
Tbetz <tobiasbetz@hotmail.de>
2022-03-24 20:08:32 +0100

    Update peerValidator.jsx

--- src/components/peerValidator.jsx
+++ src/components/peerValidator.jsx
@@ -14,6 +14,9 @@ export default class PeerValidator extends Component {
   createPeer() {
     this.setState({ loading: true });
     const peer = new Peer(`${baseString}-${this.state.ownName}`, {
+      host: "3f7e15ba-9164-450a-a050-3515dbe9f5ea.ul.bw-cloud-instance.org",
+      port: 9000,
+      path: "/myapp",
       config: {
         iceServers: [
           {
